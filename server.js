a582ba223a4252a1df7920f5a9a0bf3a6a63c2cc
Sara Binns <sarabinns@gmail.com>
2013-12-19 10:37:22 -0800

    Working server and db

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 var express = require('express');
-var app = express.createServer();
+var app = express();
+var Sequelize = require('sequelize');
+var pg = require('pg').native;
+var config = require('./db_config.js');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+	host: config.host,
+	port: config.port,
+	logging: config.logging,
+	maxConcurrentQueries: config.maxConcurrentQueries,
+	dialect: config.dialect,
+	native: config.native	
+});
+
+var voteTable = sequelize.define('votes', {
+	video_id: Sequelize.STRING,
+	timestamp: Sequelize.DATE,
+	vote: Sequelize.INTEGER
+});
+
+voteTable.sync().success(function(){
+	console.log("I'm working wjklhatajsjasl");
+}).error(function(error){
+	console.log(error);
+});
 
 console.log('serving static files from: ', __dirname);
 
