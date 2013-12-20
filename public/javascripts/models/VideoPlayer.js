2465d1844fb349440d2513efee61eabee4569e70
Joao Stein <joaosteing@gmail.com>
2013-12-20 15:58:43 -0800

    Render videos using VideoPlayer model, view and controller

--- public/javascripts/models/VideoPlayer.js
+++ public/javascripts/models/VideoPlayer.js
@@ -1,5 +1,19 @@
 App.Models.VideoPlayer = Backbone.Model.extend({
   initialize: function () {
     this.set('votes', new App.Collections.VoteList);
+  },
+
+  getVideo: function () {
+    var url = 'http://www.youtube.com/v/' + this.id + '?enablejsapi=1&playerapiid=' + this.id + '&version=3';
+
+    var params = {
+      allowScriptAccess: 'always'
+    };
+
+    var attrs = {
+      id: this.id
+    };
+
+    swfobject.embedSWF(url, this.id, '425', '356', '8', null, null, params, attrs);
   }
 });
--- public/javascripts/views/VideoPlayer.js
+++ public/javascripts/views/VideoPlayer.js
@@ -1,3 +1,17 @@
 App.Views.VideoPlayer = Backbone.View.extend({
+  initialize: function () {
+    this.collection.on('add', this.appendVideoTag, this);
+    this.render();
+  },
 
+  template: _.template( $('#videoTemplate').html() ),
+
+  render: function () {
+    this.collection.each(this.appendVideoTag, this);
+  },
+
+  appendVideoTag: function (videoPlayer) {
+    this.$el.append(this.template( videoPlayer.toJSON() ));
+    videoPlayer.getVideo();
+  }
 });
