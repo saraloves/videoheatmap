ab32b118b0a8541042c21c0ec44c1337bcc6c0b5
Joao Stein <joaosteing@gmail.com>
2013-12-20 16:41:26 -0800

    Append each video html immediately to body after creating the template for each view

--- public/javascripts/views/VideoPlayer.js
+++ public/javascripts/views/VideoPlayer.js
@@ -1,4 +1,7 @@
 App.Views.VideoPlayer = Backbone.View.extend({
+  tagName: 'section',
+  className: 'videos-wrapper',
+
   initialize: function () {
     this.collection.on('add', this.appendVideoTag, this);
     this.render();
@@ -12,6 +15,7 @@ App.Views.VideoPlayer = Backbone.View.extend({
 
   appendVideoTag: function (videoPlayer) {
     this.$el.append(this.template( videoPlayer.toJSON() ));
+    this.$el.appendTo('body');
     videoPlayer.getVideo();
   }
 });
