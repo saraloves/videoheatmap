c73eac76d6055ab1466f88c0b7e8660508d576b7
Joao Stein <joaosteing@gmail.com>
2013-12-21 11:51:56 -0800

    Add event listener to fire a function when user clicks a vote button and creates a new Vote model on VoteList collection using information from the Video model

--- public/javascripts/views/VideoPlayer.js
+++ public/javascripts/views/VideoPlayer.js
@@ -1,11 +1,31 @@
 App.Views.VideoPlayer = Backbone.View.extend({
+
   template: _.template( $('#videoTemplate').html() ),
+
+  className: 'video',
 
   initialize: function () {
     this.render();
   },
 
+  events: {
+    'click .btn': 'createVote'
+  },
+
   render: function () {
     this.$el.append(this.template( this.model.toJSON() ));
+  },
+
+  createVote: function (e) {
+    var id = this.model.id;
+    var timeStamp = this.model.player.getCurrentTime();
+    var type = $(e.target).data('vote');
+    var vote = type === 'dislike' ? -1 : 1;
+
+    this.model.attributes.votes.add({
+      videoId: id,
+      timeStamp: timeStamp,
+      vote: vote
+    });
   }
 });
