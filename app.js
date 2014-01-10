de9e0fb0b3208e6b4f198867857a3b91fbc0e107
Joao Stein <joaosteing@gmail.com>
2014-01-10 10:52:46 -0800

    Add favicon

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon( path.join(__dirname, 'public/images/favicon.ico') ));
 app.use(express.logger('dev'));
 app.use(express.cookieParser('qwertyyui'));
 app.use(express.json());
