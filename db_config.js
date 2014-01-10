3b857bfbf36ed4bf807e68638985bfbe8cf31849
Lindsey Smith <lindseyengineer@gmail.com>
2014-01-10 10:03:02 -0800

    deploy branch only

---
+++ db_config.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize');
+var pg = require('pg').native;
+
+module.exports = {
+  database: 'd58ofpt15h4ead', 
+  username: 'sqriukmoikntvc', 
+  password: 'Obgu3qMoNOlCOKXYq0-FBBpsUX',
+  host: 'ec2-54-204-43-139.compute-1.amazonaws.com',
+  port: 5432,
+  logging: false,
+  maxConcurrentQueries: 100, 
+  dialect: 'postgres',
+  native: true
+};
