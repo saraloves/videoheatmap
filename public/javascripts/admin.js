4cccc2f18123b67e172c2721437d2f4620e5b19f
Joao Stein <joaosteing@gmail.com>
2014-01-09 21:49:40 -0800

    Cache userVideos selector and move out of for loop for performance

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -8,8 +8,9 @@ $(function(){
   });
 
   var appendInfo = function(data){
+    var userVideos = $('.user-videos');
     for (var i = 0; i < data.length; i++) {
-      $('.user-videos').append('<li><span class=\'vid-link\'><a href=/link/'+ data[i].user_id + '/' + data[i].id + '>' + data[i].video_title +'</a></span><span>' + 'Share your video: <a href=/link/share/' + data[i].user_id + '/' + data[i].id + '>/link/share/' + data[i].user_id + '/' + data[i].id + '</a></span></li>');
+      userVideos.append('<li><span class=\'vid-link\'><a href=/link/'+ data[i].user_id + '/' + data[i].id + '>' + data[i].video_title +'</a></span><span>' + 'Share your video: <a href=/link/share/' + data[i].user_id + '/' + data[i].id + '>/link/share/' + data[i].user_id + '/' + data[i].id + '</a></span></li>');
     }
   };
 
